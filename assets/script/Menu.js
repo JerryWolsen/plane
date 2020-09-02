3e3d680140bc486277e536ae0b107364a41f137f
stephen.tian <stephen.tian@homecredit.cn>
2020-09-02 13:36:02 +0800

    修改主页ui

--- assets/script/Menu.js
+++ assets/script/Menu.js
@@ -3,8 +3,6 @@ cc.Class({
 
     properties: {
         bgNode: cc.Node,
-        backgroundPrefab: cc.Prefab,
-        plane: cc.Node,
         player1: cc.SpriteFrame,
         player2: cc.SpriteFrame
 
@@ -12,13 +10,13 @@ cc.Class({
 
     onLoad: function () {
         let plane
-        this.addBackground();
-        if(window.Player === window.PLAYER_1) {
-            plane = this.player1
-        } else {
-            plane = this.player2
-        }
-        this.plane.getComponent('cc.Sprite').spriteFrame = plane
+        // this.addBackground();
+        // if(window.Player === window.PLAYER_1) {
+        //     plane = this.player1
+        // } else {
+        //     plane = this.player2
+        // }
+        // this.plane.getComponent('cc.Sprite').spriteFrame = plane
     },
 
     addBackground: function(){
